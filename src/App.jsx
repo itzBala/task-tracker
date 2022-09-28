dbcf49f035e5b5b87e64b5e0de1fcde549eb4235
Your Name <you@example.com>
2022-09-28 15:44:22 +0530

    Task Adding Form

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react"
 
 import Header from "./components/Header"
 import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
 
 function App() {
   const [tasks, setTasks] = useState([
@@ -25,6 +26,12 @@ function App() {
     }
   ])
 
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1
+    const newTask = {id, ...task}
+    setTasks([...tasks, newTask])
+  }
+
   const deleteTask = (id) => {
     setTasks(tasks.filter((task) => task.id !== id))
   }
@@ -36,6 +43,7 @@ function App() {
   return (
     <div className="container">
       <Header />
+      <AddTask addTask={addTask}/>
       {tasks.length > 0 ? <Tasks tasks={tasks} deleteTask={deleteTask} toggleReminder={toggleReminder} /> : 'No Tasks'}
     </div>
   );
